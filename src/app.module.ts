6bea11214ae5891685cce02d28e18bd0ba50ccad
Robert Santos <robertdahora@gmail.com>
2024-08-28 12:29:56 -0300

    chore: configure use of env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UploadModule } from './modules/upload/upload.module';
+import { GeminiModule } from './modules/gemini/gemini.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), UploadModule, GeminiModule],
   controllers: [AppController],
   providers: [AppService],
 })
