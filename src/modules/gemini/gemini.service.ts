5eafcce703cabc106d57900c8ee262cf8ff66cde
Robert Santos <robertdahora@gmail.com>
2024-08-30 21:44:33 -0300

    refactor: update prompt

--- src/modules/gemini/gemini.service.ts
+++ src/modules/gemini/gemini.service.ts
@@ -17,7 +17,7 @@ export class GeminiService {
   async generateTextFromImage(base64File: string): Promise<number> {
     try {
       const prompt =
-        'Analyze the image and tell me the current measurement, output only the value as a integer using decimal point';
+        'Analyze the image and tell me the current measurement, output only the value as a integer';
       const imageParts = createImageParts(base64File);
 
       const result = await this.visionModel.generateContent([
